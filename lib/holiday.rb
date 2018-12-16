6fa2c6068359c44e5c6fc29f0f2f42fbc72f77a3
Whitny Edwards <wne4e2@gmail.com>
2018-12-16 19:55:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -23,16 +23,8 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-all_items = []
-  holiday_hash[:winter].collect do |supply|
-   supply.each do |items|
-      if holiday_hash[:winter][items] != nil 
-        all_items.concat(holiday_hash[:winter][items])
-      end
-   end
-  end
-  all_items
-end
+holiday_hash(:winter).values.flatten
+end 
 
 def all_supplies_in_holidays(holiday_hash)
 holiday_hash.collect do |season, v|
